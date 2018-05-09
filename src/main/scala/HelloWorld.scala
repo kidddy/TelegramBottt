8547b8e0d7ec355b4a884cbf071648ace0415b95
qxid <qxidddy@gmail.com>
2018-05-09 23:39:21 +0500

    Some work done

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,20 @@
-object HelloWorld{
-  def main(args: Array[String]) {
-    println("Hello, world!")
-  }
+//object HelloWorld{
+//  def main(args: Array[String]) {
+//    println("Hello, world!")
+//  }
+//}
+
+import java.text.SimpleDateFormat
+
+import scala.util.parsing.combinator.RegexParsers
+
+val formatter = new SimpleDateFormat("hh:m:ss yy:MM:dd")
+object CommandParser extends RegexParsers{
+  def commandName: Parser[String] = """([a-zA-Z_])""".r ^^ {_.toString}
+  def longCommandname: Parser[String] = "(" ~> commandName <~ ")" ^^ {_.toString}
+  def anonimity: Parser[Boolean] = ("yes" | "no") ^^ (_ == "yes")
+  def visibility: Parser[String] = "afterstop" | "continuous" ^^ {_.toString}
+  def date: Parser[String] = """(\d\d:\d\d:\d\d:\d\d \d\d:\d\d:\d\d)""" ^^
+    {new SimpleDateFormat("hh:mm:ss yy:MM:dd").parse(_)}
+  def id: Parser[String] = """\d+""".r ^^ {_.toInt}
 }
